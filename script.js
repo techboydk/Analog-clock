0b854eadc45175275cec58ca9375e9d8aa9d16de
Dipesh Yadav <73176903+techboydk@users.noreply.github.com>
2022-02-11 13:45:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
-function rotation(a,b,c){
-  document.getElementById("hour").style.transform = "rotate("+a+"deg)";
-document.getElementById("myDIV").style.transform = "rotate("+b+"deg)";
-  
+function timeshow(a, b, c) {
+    var Ampm;
+    var time;
+    if (a < 12) {
+        Ampm = "AM";
+    } else {
+        Ampm = "PM";
+    }
+
+    if (b < 10) {
+        b = "0" + b;
+    }
+    if (c < 10) {
+        c = "0" + c;
+    }
+    if (a == 12) {
+        time = a + " : " + b + " : " + c;
+    } else {
+        a = a % 12;
+        if (a < 10) {
+            a = "0" + a;
+        }
+        var time = a + " : " + b + " : " + c;
+    }
+
+
+    time = time.bold();
+    Ampm = Ampm.bold();
+    document.getElementById('time').innerHTML = time;
+    document.getElementById('ampm').innerHTML = Ampm;
 }
+
 setInterval(() => {
-  d = new Date();
-  hr = d.getHours();
-  mn = d.getMinutes();
-  sc = d.getSeconds();
-  h = hr*30 + (mn/2);
-  m = mn*6;
-  s = sc*6;
-  hour.style.transform = "rotate("+h+"deg)";
-  minute.style.transform = "rotate("+m+"deg)";
-  second.style.transform ="rotate("+s+"deg)";
-},1000)
+    d = new Date();
+    hr = d.getHours();
+    mn = d.getMinutes();
+    sc = d.getSeconds();
+    h = hr * 30 + (mn / 2);
+    m = mn * 6;
+    s = sc * 6;
+    hour.style.transform = "rotate(" + h + "deg)";
+    minute.style.transform = "rotate(" + m + "deg)";
+    second.style.transform = "rotate(" + s + "deg)";
+    timeshow(hr, mn, sc);
+}, 1000);
